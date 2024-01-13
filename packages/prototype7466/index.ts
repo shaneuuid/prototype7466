e71b3ebff51ed8830ce5e792cd9121ce4531d213
Shane <95544653+shaneuuid@users.noreply.github.com>
2024-01-13 18:14:44 -0500

    Add ts-ignore

--- packages/prototype7466/index.ts
+++ packages/prototype7466/index.ts
@@ -21,25 +21,31 @@ export function tree() {}
 export function cupcake() {}
 
 export function featureZ() {
+  //@ts-ignore
   console.log('featureZ');
 }
 
 export function featureY() {
+  //@ts-ignore
   console.log('feature Y');
 }
 
 export function tesdt() {
+  //@ts-ignore
   console.log('oops');
 }
 
 export function featureC() {
+  //@ts-ignore
   console.log('feature c');
 }
 
 export function waffles() {
+  //@ts-ignore
   console.log('waffles');
 }
 
 export function pancakes() {
+  //@ts-ignore
   console.log('pancakes');
 }
